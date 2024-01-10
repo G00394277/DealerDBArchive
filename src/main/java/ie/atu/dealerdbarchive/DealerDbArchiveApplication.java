23ee70959a0b3b1d2d306d0a69488c4666593bc3
G00394277 <G00394277@atu.ie>
2024-01-10 23:53:01 +0000

    Initial commit

---
+++ src/main/java/ie/atu/dealerdbarchive/DealerDbArchiveApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.dealerdbarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerDbArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerDbArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/dealerdbarchive/DealerDbArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.dealerdbarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerDbArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
